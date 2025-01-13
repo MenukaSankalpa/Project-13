bd1aa60ff16e95fe346fb5bea2d57732ebbb9589
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-13 12:27:36 +0530

    make snake class and draw it

--- snake.py
+++ snake.py
@@ -22,15 +22,26 @@ class Food:
         x = random.randint(0, number_of_cells -1)
         y = random.randint(0, number_of_cells -1)
         position = Vector2(x, y)
-        return position           
+        return position  
+    
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
+        
+    def draw(self):
+        for segment in self.body:
+            segment_rect = (segment.x*cell_size, segment.y*cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)     
+                 
 
 screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells)) 
 
-pygame.display.set_caption("retro Snake")
+pygame.display.set_caption("Retro Snake")
 clock = pygame.time.Clock()
 
 
 food = Food()
+snake =Snake()
 food_surface = pygame.image.load("Graphics/food.png")
 
 
@@ -42,6 +53,7 @@ while True:
     #drawing
     screen.fill(GREEN)   
     food.draw()
+    snake.draw()
          
     pygame.display.update()
     clock.tick(60)        
