e63971920bab5f24b2d9ef141cd59226edcc578a
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-13 12:34:43 +0530

    made snake movement(without user handle option)

--- snake.py
+++ snake.py
@@ -27,12 +27,16 @@ class Food:
 class Snake:
     def __init__(self):
         self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
+        self.direction = Vector2(1, 0)
         
     def draw(self):
         for segment in self.body:
             segment_rect = (segment.x*cell_size, segment.y*cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)     
-                 
+    
+    def update(self):
+        self.body = self.body[:-1]
+        self.body.insert(0, self.body[0] + self.direction)             
 
 screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells)) 
 
@@ -44,12 +48,18 @@ food = Food()
 snake =Snake()
 food_surface = pygame.image.load("Graphics/food.png")
 
+SNAKE_UPDATE = pygame.USEREVENT
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             pygame.quit()
             sys.exit()
+    
+    snake.update() 
+     
+            
     #drawing
     screen.fill(GREEN)   
     food.draw()
