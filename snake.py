2c27a870c55eecf46fecce6598d28d42b3dfd153
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-13 08:24:16 +0530

    create game loop

--- snake.py
+++ snake.py
@@ -9,4 +9,11 @@ pygame.display.set_caption("retro Snake")
 clock = pygame.time.Clock()
 
 
-  
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
+    clock.tick(60)        
